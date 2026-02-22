7d24deb465fdf49531c34f5511ca28f408af9b34
aakashsingghh <akashsingh33280@gmail.com>
2026-02-22 23:31:19 +0530

    add XML migration state files and implement DHT sensor setup and reading functionality

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,25 @@
 #define DHTPIN 2
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
-// void setup() {
-//     Serial.begin(9600);
-//     dht.begin();
-//     Serial.println("Initalizing DHT sensor!");
-//
-// }
-//
-// void loop() {
-//     // write your code here
-//     float humidity = dht.readHumidity();
-//     float temperature = dht.readTemperature();
-//     if (isnan(humidity) || isnan(temperature)) {
-//         Serial.println("Failed to read from DHT sensor!");
-//         delay(2000);
-//         return;
-//     }
-//     Serial.println("Humidity: ");
-//     Serial.println(humidity);
-//     Serial.println("Temperature: ");
-//     Serial.println(temperature);
-//     delay(2000);
-// }
+void setup() {
+    Serial.begin(9600);
+    dht.begin();
+    Serial.println("Initalizing DHT sensor!");
+
+}
+
+void loop() {
+    // write your code here
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+    if (isnan(humidity) || isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        delay(2000);
+        return;
+    }
+    Serial.println("Humidity: ");
+    Serial.println(humidity);
+    Serial.println("Temperature: ");
+    Serial.println(temperature);
+    delay(2000);
+}
